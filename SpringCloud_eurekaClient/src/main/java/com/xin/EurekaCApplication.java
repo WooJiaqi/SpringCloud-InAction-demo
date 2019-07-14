b35653ad9171d6eb5e516fe4890fc6bedba442ec
GakiWu <leeseenlian@hotmail.com>
2019-07-14 23:48:17 +0800

    create springcloud config project update springcloud Eureka client(service provider)

--- SpringCloud_eurekaClient/src/main/java/com/xin/EurekaCApplication.java
+++ SpringCloud_eurekaClient/src/main/java/com/xin/EurekaCApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * provider 服务提供方
+ */
 @SpringBootApplication
 @EnableDiscoveryClient //注册服务发现
 public class EurekaCApplication {
